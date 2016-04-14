61ec7ffa1bae1eb6a5ebb3b9704df1bb33c518b1
JbIPS <jeanbaptiste.richardet@gmail.com>
2016-04-14 18:26:04 +0200

    Adds torrent & notify

--- samples/client.js
+++ samples/client.js
@@ -1,22 +1,26 @@
 'use strict';
 
+const Https = require('https');
+
 const Transmission = require('transmission');
+const request = require('request');
+const Promise = require('bluebird');
 
 const EZTV = require('../lib/eztvClient');
 const config = require('./config.json');
 
 const transmission = new Transmission({
-    port: 9091,
-    host: '127.0.0.1',
-    username: 'username',
-    password: 'password'
+    port: config.transmission.port || 9091,
+    host: config.transmission.host,
+    username: config.transmission.username,
+    password: config.transmission.password
 });
 
 let eztv = new EZTV();
 eztv.getTodayReleases()
 .then(function(results){
-	let newEpisodes = false;
-	results.filter(ep => {
+	let newEpisodes = [];
+	Promise.all(results.filter(ep => {
 		let i = 0;
 		let matched = false;
 		while(i < config.shows.length && !matched){
@@ -30,28 +34,47 @@ eztv.getTodayReleases()
 				matched = ep.show.proper == watchedShow.proper;
 				matched = ep.show.repack == watchedShow.repack;
 
-				if(matched) newEpisodes = true;
+				if(matched) newEpisodes.push(ep.show.title);
 			}
 			i++;
 		}
 		return i < config.shows.length;
 	})
-  .forEach(ep => {
-		addTorrent(ep.magnet);
-	});
-
-	if(!newEpisodes) console.info('No new episode today.');
+  .map(ep => {
+		return addTorrent(ep.magnet);
+	}))
+  .then(() => {
+    if(newEpisodes.length == 0) console.info('No new episode today.');
+    else notify(newEpisodes);
+  });
 })
 .catch(console.error);
 
 // Add a torrent by passing a URL to .torrent file or a magnet link
 function addTorrent(url){
+  return new Promise((reject, resolve) => {
     transmission.addUrl(url, function(err, result) {
         if (err) {
-            return console.log(err);
+            return reject(err);
         }
-        var id = result.id;
-        console.log('Just added a new torrent.');
-        console.log('Torrent ID: ' + id);
+        resolve(result.id);
     });
+  });
+}
+
+function notify(newEpisodes){
+  var postData = {
+    form : {
+      apikey: config.nma.apikey,
+      application: 'AutoDL',
+      event: newEpisodes.length + ' new episode'+ newEpisodes.length > 1 ? 's are':'is' + ' downloading!',
+      description: newEpisodes.join(', ') + newEpisodes.length > 1 ? 'are':'is'+ ' being download'
+    }
+  };
+
+  request.post('https://www.notifymyandroid.com/publicapi/notify', postData, (err, res, body) => {
+    if(err) console.error(err);
+
+    else console.log(body);
+  });
 }
